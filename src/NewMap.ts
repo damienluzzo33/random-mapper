3c35fc911c5c0e8b3d8e0e5e68876ca89bc9797e
Damien Luzzo <damienluzzo33@gmail.com>
2022-01-07 19:31:56 -0600

    add map marker functionality and set up interface for addMarker method

--- src/NewMap.ts
+++ src/NewMap.ts
@@ -1,14 +1,35 @@
+//* here are our "instructions" - aka the interface - for all of our classes that tells them how they can be used as arguments to our addMarker method below
+interface MapItem {
+    location: {
+        latitude: number;
+        longitude: number;
+    }
+}
 
 export class NewMap {
+    //* set the googleMap to be private to prevent tampering
     private googleMap: google.maps.Map;
-    
+    //* constructor function takes in a divId variable    
     constructor(divId: string) {
         this.googleMap = new google.maps.Map(document.getElementById(divId), {
+            //* there are two options we want to use here: the "zoom" and "center" options
             zoom: 4, 
             center: { 
+                //* center the map on the United States to start
                 lat: 37.0902,
                 lng: -95.7129
             }
         });
+    }
+
+    //* Marker --> options: { map: what map we want to add marker to, position: where the marker shows up on the map itself --> lat/lng } 
+    addMarker(mapItem: MapItem): void {
+        new google.maps.Marker({
+            map: this.googleMap,
+            position: {
+                lat: mapItem.location.latitude,
+                lng: mapItem.location.longitude
+            }
+        })
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 /// <reference types="@types/google.maps" />
 
-// import { User } from "./User";
-// import { Company } from "./Company";
+//* bring in the classes we are using
+import { User } from "./User";
+import { Company } from "./Company";
 import { NewMap } from "./NewMap";
 
-new NewMap("map");
+//* create instances of all of our classes
+const newUser = new User();
+const newCompany = new Company();
+const newMap = new NewMap("map");
 
+//* add markers to our map with our instances of User and Company
+newMap.addMarker(newUser);
+newMap.addMarker(newCompany);
